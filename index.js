a407569c1f233ef6298b576298f31f21a99d5369
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2022-03-29 10:02:07 -0700

    Create file with the text in it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios').default
 const cheerio = require('cheerio')
+const fs = require('fs').promises
 
 const URL = 'https://presidente.gob.mx/secciones/version-estenografica/'
 
@@ -17,10 +18,27 @@ async function getLinks() {
 }
 
 async function createFile(link, header, content) {
+  // example link: https://presidente.gob.mx/28-03-22-version-estenografica-de-la-conferencia-de-prensa-matutina-del-presidente-andres-manuel-lopez-obrador
+  const filename = link.replace(/\/$/, '').split('/').pop()
+  const extension = 'txt'
+  const basePath = './data'
+
+  const match = filename.match(/^\d{2}-\d{2}-\d{2}/)
+  if (match === null) {
+    console.log(`cannot find date in filename: ${filename}`)
+    return
+  }
+
+  const [ _, month, year ] = filename.split('-')
+  const path = `${basePath}/20${year}/${month}`
+
+  const fullPathName = `${path}/${filename}.${extension}`
+  const fullContent = `${header}\n${content}`
   try {
-
+    await fs.mkdir(path, { recursive: true })
+    await fs.writeFile(fullPathName, fullContent, { flag: 'wx' })
   } catch (error) {
-
+    console.log('Error: ', error)
   }
 }
 
@@ -51,8 +69,8 @@ async function init() {
 
   // console.log(links)
 
-  // processLink(links[0])
-  processLinks(links)
+  processLink(links[0])
+  // processLinks(links)
 }
 
 init()
